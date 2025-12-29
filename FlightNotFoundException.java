685f5c8ecff6eb687032aadc3f1d9233263e5a1a
Dev Sathwik P <82132397+Sathwik3010@users.noreply.github.com>
2025-12-29 12:43:05 +0530

    Exceptions

---
+++ FlightNotFoundException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+@SuppressWarnings("serial")
+public class FlightNotFoundException extends Exception {
+
+	public FlightNotFoundException(String message) {
+		super(message);
+	}
+	
+	
+}
